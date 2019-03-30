f85e8a8299f43636a29d9e9590181b3ab810c67b
Eric Boutilier <eboutili@users.noreply.github.com>
2019-03-30 18:05:15 -0500

    Update demo_splunk.sh

--- demo_splunk.sh
+++ demo_splunk.sh
@@ -2,7 +2,6 @@
 ## Tested on centos 7
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-# Pin the docker-ce version for now because of a bug in 18.09 (shipping as latest as of 3/25/19)
 yum install -y docker-ce-18.06.2.ce-3.el7
 systemctl enable docker
 systemctl start docker
