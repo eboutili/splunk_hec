1faba6898b0542d666aa6d0279e6fde917bc1c54
Eric Boutilier <eboutili@users.noreply.github.com>
2019-04-12 14:10:19 -0500

    Update demo_splunk.sh

--- demo_splunk.sh
+++ demo_splunk.sh
@@ -12,11 +12,7 @@ FROM splunk/splunk:latest
 RUN sudo apt-get update && sudo apt-get install -y git
 ENV SPLUNK_START_ARGS --accept-license
 ENV SPLUNK_PASSWORD puppetlabs
-ENV GITHUB_URL https://github.com/puppetlabs
-ENV APP3 TA-puppet-report-viewer
 EOF
 docker build -t splunkdemo_i .
 docker run --name splunkdemo_c --restart always -d -p 8000:8000 -p 8088:8088 splunkdemo_i
-docker exec splunkdemo_c bash -c 'sudo git clone "${GITHUB_URL}/${APP3}.git" "/opt/splunk/etc/apps/${APP3}"'
-
-
+docker exec splunkdemo_c bash -c 'sudo wget https://github.com/puppetlabs/TA-puppet-report-viewer/archive/1.3.5.tar.gz -O - | gunzip -c - | tar -C /opt/splunk/etc/apps -xf -'
